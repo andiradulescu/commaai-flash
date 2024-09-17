a5c9bd982c158a214e1d5e97f14ab805d49f0457
Andrei Radulescu <andi.radulescu@gmail.com>
2024-09-17 20:51:36 +0300

    ci pr-376

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ const config = {
   manifests: {
     release: 'https://raw.githubusercontent.com/commaai/openpilot/release3/system/hardware/tici/agnos.json',
     master: 'https://raw.githubusercontent.com/commaai/openpilot/master/system/hardware/tici/agnos.json',
+    ci: 'https://gitlab.com/commaai/ci-artifacts/-/raw/agnos-builder/pr-376/agnos.json',
   },
   loader: {
     url: 'https://raw.githubusercontent.com/commaai/flash/master/src/QDL/sdm845_fhprg.bin',
